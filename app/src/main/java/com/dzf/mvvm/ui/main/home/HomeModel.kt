ef494dd599c5eaca540b4126c4e6f1fab6557723
cortana521 <874092094@qq.com>
2023-02-14 13:52:48 +0800

    更新

--- app/src/main/java/com/dzf/mvvm/ui/main/home/HomeModel.kt
+++ app/src/main/java/com/dzf/mvvm/ui/main/home/HomeModel.kt
@@ -22,7 +22,7 @@ import com.dzf.mvvm.utils.SysUtils
  * @ProjectName : MVVM
  * @Author : Dai Zhi Feng
  * @Time : 2021/9/1 16:03
- * @Description : 文件描述
+ * @Description : 首页数据处理
  */
 class HomeModel : BaseViewModel<FragmentHomeBinding>() {
 
